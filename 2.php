029af94bf1a698ffb142c77c67f3cbe1787cf81b
peng <pyj505926697@163.com>
2020-04-23 19:33:46 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,27 +1,23 @@
-<?php
-//复制目录
-//mkdir 创建文件
-//copy() 复制文件
-//mkdir('./xxoo', 0777, true);
+第五题：
+$a = "CodeReviewStackExchange";
 
-//$dir = rtrim($dir,'/').'/';
-//$bir = rtrim($bir,'/').'/';
-//打开目录
-$res = copy('./copy.php','2.php');
-var_dump($res);
-$hd = opendir('./images');
-//循环每个目录
-//var_dump(dirname(__FILE__));
-$path = dirname(__FILE__);
-while($f = readdir($hd)){
-    if($f =='.'||$f == '..'){continue;}
-    //echo $f.'<br/>';
-    //echo './abc'.$f,'<br>';
-    var_dump('./'.$f);
-    var_dump($path.'/xxoo/'.$f);
-    //echo '<br/>';
-    copy('./images/'.$f,'./xxoo/'.$f);
+function code_review($a) {
+    // 按照
+    $a = preg_replace('/(?<!^)([A-Z])/', '-\\1', $a);
+    
+    $a = explode('-', $a);
+    
+    $res = '';
+    
+    foreach($a as $k => $v) {
+        $res .= (strtolower($v)) . '_';
+    }
+    
+    
+    return rtrim($res, "_");
 
 }
-//关闭文件
-closedir($hd);
+
+$r = code_review($a);
+
+var_dump($r);
